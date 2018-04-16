55ace93b26af915fc9bcb0acdaaf9d66bb4ab7af
Jack <jacktbader5@gmail.com>
2018-04-16 10:40:47 -0600

    Removing puts

--- mdd.rb
+++ mdd.rb
@@ -8,28 +8,21 @@ data = JSON.parse(response)
 
 $score = 0
 
-test = data.map do |u|
+data.map do |u|
  if u['type'] == 'IssuesEvent'
    $score = $score + 7
-   puts 'IssuesEvent'
  elsif u['type'] == 'IssueCommentEvent'
    $score = $score + 6
-   puts 'IssueCommentEvent'
  elsif u['type'] == 'PushEvent'
    $score = $score + 5
-   puts 'PushEvent'
  elsif u['type'] == 'PullRequestReviewCommentEvent'
    $score = $score + 4
-   puts 'PullRequestReviewCommentEvent'
  elsif u['type'] == 'WatchEvent'
    $score = $score + 3
-   puts 'WatchEvent'
  elsif u['type'] == 'CreateEvent'
    $score = $score + 2
-   puts 'CreateEvent'
  else
    $score = $score + 1
-   puts 'Other event'
  end
 end
 
